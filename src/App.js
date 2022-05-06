9e59a2083f461229a2a26f64a22e7a89952bd108
Tung Le <tunglungtung87@gmail.com>
2022-05-06 15:46:53 +0700

    updating a Directory page with category component and item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   // Static dump data
@@ -32,10 +31,8 @@ const App = () => {
     }
   ];
   return (
-    <div className="categories-container">
-      {categories.map((category) => {
-        return <CategoryItem key={category.id} category={category} />;
-      })}
+    <div>
+      <Directory categories={categories} />
     </div>
   );
 };
