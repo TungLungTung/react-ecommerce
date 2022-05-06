da0d94daf2bfd553e35430332db6efefda327609
Tung Le <tunglungtung87@gmail.com>
2022-05-06 18:17:19 +0700

    finished build navigation component, and using react router dom v6 to implement Routes in application

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+/// React router dom
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +11,9 @@ import reportWebVitals from './reportWebVitals';
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
