36366a88f52759a30b53e76801746fb423b0c234
Tung Le <tunglungtung87@gmail.com>
2022-05-10 20:24:54 +0700

    add new addToCart features

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,58 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+
+const addCartItem = (cartItems, productToAdd) => {
+  /// find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find((cartItem) => {
+    /// Return ture
+    return cartItem.id === productToAdd.id;
+  });
+
+  // If found, increment quantity
+  if (existingCartItem) {
+    /// REturn new array object, DO NOT mutate original object
+    return cartItems.map((cartItem) => {
+      return cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem;
+    });
+  }
+
+  // return new array with modified cartItems/ new cart items
+  /// Ben duoi la du cho ban dau chua co gio hang hay gi cung khong bi loi
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsCartOpen: () => {}
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    /// Update cart count.
+    const newCartCount = cartItems.reduce((total, cartItem) => {
+      return total + cartItem.quantity;
+    }, 0);
+    setCartCount(newCartCount);
+  }, [cartItems]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount
+  };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
