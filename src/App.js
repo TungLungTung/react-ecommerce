12d2e722e747e488fdaf655efe7def53db80a1f4
Tung Le <tunglungtung87@gmail.com>
2022-05-10 20:53:31 +0700

    implement increment, decrement quantity in checkout page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
+import CheckOut from './routes/checkout/checkout.component';
 
 import Shop from './routes/shop/shop.component';
 
@@ -14,6 +15,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<CheckOut />} />
       </Route>
     </Routes>
   );
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -22,11 +22,32 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  /// Find the cart item to remove (if exist)
+  const existingCartItem = cartItems.find((cartItem) => {
+    /// Return ture
+    return cartItem.id === cartItemToRemove.id;
+  });
+  /// Check if quantity is equal to 1, remove this item from the cart.
+  if (existingCartItem.quantity === 1) {
+    /// Keep item in callback function
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  /// return back cart items with matching cart items with reduced quantity
+  return cartItems.map((cartItem) => {
+    return cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem;
+  });
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
   cartCount: 0
 });
 
@@ -47,10 +68,15 @@ export const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
+  const removeItemFromCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
+    removeItemFromCart,
     cartItems,
     cartCount
   };
